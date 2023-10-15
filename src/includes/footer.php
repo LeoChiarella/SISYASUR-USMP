3601c471ce46b6a9097ec28bb576205d80259051
LeoChiarella <leochiarella2001@gmail.com>
2023-10-15 12:38:31 -0500

    cabecera y pie de pagina

---
+++ src/includes/footer.php
@@ -0,0 +1,13 @@
+
+<script src="../assets/js/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+<script src="../assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="../assets/js/scripts.js"></script>
+<script src="../assets/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="../assets/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="../assets/js/sweetalert2.all.min.js"></script>
+<script src="../assets/js/jquery-ui/jquery-ui.min.js"></script>
+<script src="../assets/js/Chart.bundle.min.js"></script>
+<script src="../assets/js/funciones.js"></script>
+</body>
+
+</html>
