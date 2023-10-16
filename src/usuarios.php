7504b168a4862a43fbdd5277e2f936b40e057b8d
Arzu <ayrton_herrera@usmp.pe>
2023-10-16 01:50:47 -0500

    vista de usuarios

---
+++ src/usuarios.php
@@ -0,0 +1,83 @@
+<?php include_once "includes/header.php";
+include "../conexion.php";
+$id_user = $_SESSION['idUser'];
+$permiso = "usuarios";
+$sql = mysqli_query($conexion, "SELECT p.*, d.* FROM permisos p INNER JOIN detalle_permisos d ON p.id = d.id_permiso WHERE d.id_usuario = $id_user AND p.nombre = '$permiso'");
+$existe = mysqli_fetch_all($sql);
+if (empty($existe) && $id_user != 1) {
+    header("Location: permisos.php");
+}
+if (!empty($_POST)) {
+    $alert = "";
+    if (empty($_POST['nombre']) || empty($_POST['correo']) || empty($_POST['usuario']) || empty($_POST['clave'])) {
+        $alert = '<div class="alert alert-danger" role="alert">
+        Todo los campos son obligatorios
+        </div>';
+    } else {
+        $nombre = $_POST['nombre'];
+        $email = $_POST['correo'];
+        $user = $_POST['usuario'];
+        $clave = md5($_POST['clave']);
+        $query = mysqli_query($conexion, "SELECT * FROM usuario where correo = '$email'");
+        $result = mysqli_fetch_array($query);
+        if ($result > 0) {
+            $alert = '<div class="alert alert-warning" role="alert">
+                        El correo ya existe
+                    </div>';
+        } else {
+            $query_insert = mysqli_query($conexion, "INSERT INTO usuario(nombre,correo,usuario,clave) values ('$nombre', '$email', '$user', '$clave')");
+            if ($query_insert) {
+                $alert = '<div class="alert alert-primary" role="alert">
+                            Usuario registrado
+                        </div>';
+                header("Location: usuarios.php");
+            } else {
+                $alert = '<div class="alert alert-danger" role="alert">
+                        Error al registrar
+                    </div>';
+            }
+        }
+    }
+}
+?>
+
+<div class="table-responsive">
+    <table class="table table-hover table-striped table-bordered mt-2" id="tbl">
+        <thead class="thead-dark">
+            <tr>
+                <th>#</th>
+                <th>Usuario</th>
+                <th>Nombre</th>
+                <th>Correo</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php ?>
+            <?php
+            include "../conexion.php";
+
+            $query = mysqli_query($conexion, "SELECT * FROM usuario ORDER BY estado DESC");
+            $result = mysqli_num_rows($query);
+            if ($result > 0) {
+                while ($data = mysqli_fetch_assoc($query)) {
+                    if ($data['estado'] == 1) {
+                        $estado = '<span class="badge badge-pill badge-success">Activo</span>';
+                    } else {
+                        $estado = '<span class="badge badge-pill badge-danger">Inactivo</span>';
+                    }
+            ?>
+                    <tr>
+
+                        <td><?php echo $data['idusuario']; ?></td>
+                        <td><?php echo $data['nombre']; ?></td>
+                        <td><?php echo $data['correo']; ?></td>
+                        <td><?php echo $data['usuario']; ?></td>
+                        
+                    </tr>
+            <?php }
+            } ?>
+        </tbody>
+    </table>
+</div>
+<?php include_once "includes/footer.php"; ?>
